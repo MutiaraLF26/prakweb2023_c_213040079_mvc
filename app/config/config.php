e2c71dff116c297d9525ffbd4471235a7d54b36a
MutiaraLF26 <mutiara.213040079@mail.unpas.ac.id>
2023-10-25 05:39:59 +0700

    pertemuan 6 mvc

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('BASEURL', 'http://localhost/prakweb2023_c_213040079_mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'phpmvc');
